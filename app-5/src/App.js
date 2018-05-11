c981f2ea28f824355dde84180a54881c8c2f4d84
Owen Ekhator <oe52@gmail.com>
2018-05-10 21:20:16 -0500

    completed

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Img from './Image.js'
 
 class App extends Component {
+
+  constructor(){
+    super();
+  this.state ={
+    img: 'http://via.placeholder.com/350x150'
+  }
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Img
+          img = {this.state.img}
+          />
         </p>
       </div>
     );
